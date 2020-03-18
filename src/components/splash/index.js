a16624c4664c8332a6bbfcad777befed8f535427
Derek Etman <dereketman@gmail.com>
2020-03-18 14:51:30 -0700

    +Added image + Fixed text card

--- src/components/splash/index.js
+++ src/components/splash/index.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Link } from "@material-ui/core";
+import derek_leaning from "./images/Leaning_no_background_bw.png";
 // import Box from "@material-ui/system";
 
 const useStyles = makeStyles(theme => ({
@@ -28,31 +29,36 @@ export function MainSplash() {
 			<div className="MainSplashContainer">
 				<Container maxWidth="xl" className="Splash_Container">
 					<Grid container spacing={3}>
-						<Grid item xs={12}>
+						<Grid item xs={7}>
 							<h1 id="Splash_Hello">HELLO!</h1>
 							<h3 id="Splash_Message"> I'M DEREK</h3>
 							<h3 id="Splash_Message2"> HOW CAN I HELP?</h3>
+							<Grid item l={12} className="splash_nav_container" justify="right">
+								<Link href="#" id="splash_nav">
+									CODE
+								</Link>
+								<Link href="#" id="splash_nav">
+									MUSIC
+								</Link>
+								<Link href="#" id="splash_nav">
+									MORE
+								</Link>
+							</Grid>
 						</Grid>
 
-						<Grid item l={6} className="splash_nav_container" justify="right">
-							<Link href="#" id="splash_nav">
-								CODE
-							</Link>
-							<Link href="#" id="splash_nav">
-								MUSIC
-							</Link>
-							<Link href="#" id="splash_nav">
-								MORE
-							</Link>
+						<Grid item xs={5}>
+							<img src={derek_leaning} />
 						</Grid>
 					</Grid>
 				</Container>
+
+				<Container></Container>
 			</div>
 			<div className="splash_text_container">
-				<Container maxWidth="xl" >
+				<Container maxWidth="xl">
 					<Grid>
-						<Paper elevation={2} className="splash_text">
-							<p >
+						<Paper elevation={3} className="splash_text">
+							<p>
 								Experienced freelance with a demonstrated history of working in the
 								media production industry. Skilled in all aspects of audio, digital
 								photography, film production, and music composition. Strong professional
