424a529cf4dcd190b252143f52cc2d0891c1b9c9
Derek Etman <dereketman@gmail.com>
2020-03-19 16:04:01 -0700

    + Updated Styling on Sound projects

--- src/components/soundProjects/index.js
+++ src/components/soundProjects/index.js
@@ -5,6 +5,9 @@ export default function soundPage(){
     return(
         <>
         <h1>Home page for all things audio</h1>
+        <br/>
+        <br/>
+        <h2>~Coming soon~</h2>
         </>
     )
 }
