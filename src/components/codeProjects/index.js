e85fea5a9069752f1d74a2990173dc06c3bfc33c
Derek Etman <dereketman@gmail.com>
2020-03-19 16:20:37 -0700

    + Initialized project card.

--- src/components/codeProjects/index.js
+++ src/components/codeProjects/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+
+const testData=[
+    {   id: 0,
+        name:"test",
+        project_info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        project_url: "www.testUrl.com",
+        git_repo:"www.gitrepo.com",
+    }
+]
+
 export default function codePage(){
     return(
         <>
