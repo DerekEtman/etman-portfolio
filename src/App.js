f180997a4565505054351cfa7b0f08ca14a0ec1d
Derek Etman <dereketman@gmail.com>
2020-03-19 09:50:53 -0700

    +Added place holders for pages +Set up routes +Linked menu

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import logo from './logo.svg';
 import './App.scss';
 import { NavBar } from './components/navbar/navbar';
 import { MainSplash } from './components/splash';
+import ContactPage from './components/contact/contactPage';
+
+import {Switch, Route} from 'react-router-dom';
+import aboutPage from './components/about/aboutPage';
+import soundPage from './components/soundProjects';
+import codePage from './components/codeProjects';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <MainSplash />
+    <Switch>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/" component={MainSplash}/>
+        <Route path="/code" component={codePage}/>
+        <Route path="/sound" component={soundPage}/>
+        <Route path="/about" component={aboutPage}/>
+        <Route path="/contact" component={ContactPage} />
 
-    </div>
+      </div>
+    </Switch>
   );
 }
 
---
+++ src/components/soundProjects/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function soundPage(){
+    return(
+        <>
+        <h1>Home page for all things audio</h1>
+        </>
+    )
+}
