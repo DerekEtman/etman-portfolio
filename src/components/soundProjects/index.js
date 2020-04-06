44d09a325ea6e62d155cc61628306a929ae345b3
Derek Etman <dereketman@gmail.com>
2020-04-06 09:20:36 -0700

    + Added construction page on sound page for now

--- src/components/soundProjects/index.js
+++ src/components/soundProjects/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+	container: {
+		backgroundColor: "Black",
+		color: "white",
+		height: "90vh",
+    },
+    textWrapper: {
+        // padding:"50px auto",
+        border:"12px dashed yellow",
+        // marginTop:50,
+    }
+}));
 
-export default function soundPage(){
-    return(
-        <>
-        <h1>Home page for all things audio</h1>
-        <br/>
-        <br/>
-        <h2>~Coming soon~</h2>
-        </>
-    )
+export default function SoundPage() {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.container}>
+			<div className={classes.textWrapper}>
+				<br />
+				<h1> Under Construction </h1>
+				<h2>Home page for all things audio</h2>
+				<h2>~Coming soon~</h2>
+				<br />
+			</div>
+		</div>
+	);
 }
