9f6bdfc773b85ed3fe1fb3a3eb9384cf86200618
Derek Etman <dereketman@gmail.com>
2020-05-11 10:05:40 -0700

    refactor: about styling

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -57,7 +57,6 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
-		backgroundColor: theme.palette.background.paper,
 		display: "flex",
 		backgroundColor: "black",
 		color: "white",
@@ -85,10 +84,10 @@ const useStyles = makeStyles((theme) => ({
 	aboutHeader: {
 		fontFamily: "'Bebas Neue', cursive",
 	},
-	aboutBodyWrapper:{
-		border:"1px solid white",
-		margin:"1rem 0",
-		borderRadius:"5px",
+	aboutBodyWrapper: {
+		border: "1px solid white",
+		margin: "1rem 0",
+		borderRadius: "5px",
 	},
 	aboutBody: {
 		padding: "1rem 0",
@@ -110,8 +109,8 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: "center",
 		backgroundColor: "black",
 		color: "white",
-		border:"1px solid white",
-		borderRadius:"5px",
+		border: "1px solid white",
+		borderRadius: "5px",
 	},
 	contactAddresses: {
 		// padding: "2%",
@@ -128,15 +127,13 @@ const useStyles = makeStyles((theme) => ({
 	contactText: {
 		color: "white",
 	},
-	techStackSkills:{
-		border: "none",
+	techStackSkills: {
 		boxShadow: "none",
-		backgroundColor:"black",
-		color:"white",
-		border:"1px solid white",
-		borderRadius:"5px",
-
-	}
+		backgroundColor: "black",
+		color: "white",
+		border: "1px solid white",
+		borderRadius: "5px",
+	},
 }));
 
 export default function VerticalTabs() {
@@ -171,64 +168,66 @@ export default function VerticalTabs() {
 						This is me...
 					</Typography>
 					<div className={classes.aboutBodyWrapper}>
-					<Typography variant="body1" className={classes.aboutBody}>
-						As crucial as it is, the ability to create music is not enough today;
-						while I might not have been aware then, everything I've done as a musician
-						and composer has reinforced and aided in my pursuit of being a web
-						developer.
-					</Typography>
-					<Typography variant="body1" className={classes.aboutBody}>
-						I could make this really long-winded but as web developers and music
-						professionals we both know that we have lots to do and very little time to
-						do them. Here’s a really quick breakdown, if you want more details you can
-						read past that: -Collector and player of multiple instruments -Hundreds of
-						hours of recording, mixing, and editing -Tech-savvy, builds own computers,
-						problem solver - Obsessed with sound quality - Workaholic - Many Sleepless
-						nights - Quick at learning and adapting - Switched to web development
-					</Typography>
-					<Typography variant="body1" className={classes.aboutBody}>
-						Playing piano from a young age led me to compose music, collect, and
-						playing multiple instruments, I recently picked up a tremoloa and a
-						marxophone. Very fun instruments from the 20s and 30s
-					</Typography>
-					<Typography variant="body1" className={classes.aboutBody}>
-						Interning at recording studios has led to hundreds of hours of recording
-						and mixing, reading manuals for each piece of equipment, the endless quest
-						for producing better sounds, and the awesome ability of getting coffee.
-						<br />
-						Dialogue editing jobs have led to perfectionism and experience of having a
-						clean and structured file structure.
-						<br />
-						The lack of good instrumentalists around me got me interested in VSTs and
-						programming. Which meant I needed stronger computers and more efficient
-						workflows to utilize them well. This meant building stronger, faster,
-						cheaper workstations led me down the road of building my own computers.
-						<br />
-						I graduated from The California Institute of the Arts with a B.A. in Music
-						Composition with a certificate in composition in Film and Video. While
-						studying there I worked with many students on their respective projects;
-						writing music for their animation, creating a soundscape for a theater
-						piece, mixing the audio on their film project.
-						<br />
-						On the surface, the art industry and the tech industry seem worlds apart.
-						When you dig deeper, the development and flow are completely the same.
-						<br />
-						The most important role you have is to understand your
-						client/director/product manager, establishing a solid rapport and easy
-						communication. If they ask for the color blue, it’s important to find out
-						what blue they’re envisioning; they might have imagined purple instead. If
-						expectations are unrealistic, it’s up to you to find a compromise that
-						works in their favor. The amount of problem-solving and creativity are on
-						the same level but achieved using different tools.
-						<br />
-						This is why I felt comfortable switching between coding and music, I
-						really enjoy the learning process and problems facing you while coding.
-						Plus you can listen to music while coding (You can’t really listen to
-						music while working with audio…)
-						<br />
-						Since I love learning and there are so many experiences to be had with
-						coding, I’ll be here a while.
-					</Typography>
+						<Typography variant="body1" className={classes.aboutBody}>
+							As crucial as it is, the ability to create music is not enough today;
+							while I might not have been aware then, everything I've done as a
+							musician and composer has reinforced and aided in my pursuit of being a
+							web developer.
+						</Typography>
+						<Typography variant="body1" className={classes.aboutBody}>
+							I could make this really long-winded but as web developers and music
+							professionals we both know that we have lots to do and very little time
+							to do them. Here’s a really quick breakdown, if you want more details you
+							can read past that: -Collector and player of multiple instruments
+							-Hundreds of hours of recording, mixing, and editing -Tech-savvy, builds
+							own computers, problem solver - Obsessed with sound quality - Workaholic
+							- Many Sleepless nights - Quick at learning and adapting - Switched to
+							web development
+						</Typography>
+						<Typography variant="body1" className={classes.aboutBody}>
+							Playing piano from a young age led me to compose music, collect, and
+							playing multiple instruments, I recently picked up a tremoloa and a
+							marxophone. Very fun instruments from the 20s and 30s
+						</Typography>
+						<Typography variant="body1" className={classes.aboutBody}>
+							Interning at recording studios has led to hundreds of hours of recording
+							and mixing, reading manuals for each piece of equipment, the endless
+							quest for producing better sounds, and the awesome ability of getting
+							coffee.
+							<br />
+							Dialogue editing jobs have led to perfectionism and experience of having
+							a clean and structured file structure.
+							<br />
+							The lack of good instrumentalists around me got me interested in VSTs and
+							programming. Which meant I needed stronger computers and more efficient
+							workflows to utilize them well. This meant building stronger, faster,
+							cheaper workstations led me down the road of building my own computers.
+							<br />
+							I graduated from The California Institute of the Arts with a B.A. in
+							Music Composition with a certificate in composition in Film and Video.
+							While studying there I worked with many students on their respective
+							projects; writing music for their animation, creating a soundscape for a
+							theater piece, mixing the audio on their film project.
+							<br />
+							On the surface, the art industry and the tech industry seem worlds apart.
+							When you dig deeper, the development and flow are completely the same.
+							<br />
+							The most important role you have is to understand your
+							client/director/product manager, establishing a solid rapport and easy
+							communication. If they ask for the color blue, it’s important to find out
+							what blue they’re envisioning; they might have imagined purple instead.
+							If expectations are unrealistic, it’s up to you to find a compromise that
+							works in their favor. The amount of problem-solving and creativity are on
+							the same level but achieved using different tools.
+							<br />
+							This is why I felt comfortable switching between coding and music, I
+							really enjoy the learning process and problems facing you while coding.
+							Plus you can listen to music while coding (You can’t really listen to
+							music while working with audio…)
+							<br />
+							Since I love learning and there are so many experiences to be had with
+							coding, I’ll be here a while.
+						</Typography>
 					</div>
 				</Paper>
 			</TabPanel>
@@ -245,16 +244,16 @@ export default function VerticalTabs() {
 				</Typography>
 
 				<Card className={classes.skillsSection}>
-				{/* <Typography variant="h3 " className={classes.resumeSectionTitles}>Skills</Typography> */}
+					{/* <Typography variant="h3 " className={classes.resumeSectionTitles}>Skills</Typography> */}
 
-				<CardContent className={classes.techStackSkills}>
-					<Grid container spacing={0}>
-						{skills.coding.map((skill) => {
-							return <SkillCard codingSkill={skill} />;
-						})}
-					</Grid>
-				</CardContent>
-			</Card>
+					<CardContent className={classes.techStackSkills}>
+						<Grid container spacing={0}>
+							{skills.coding.map((skill) => {
+								return <SkillCard codingSkill={skill} />;
+							})}
+						</Grid>
+					</CardContent>
+				</Card>
 			</TabPanel>
 
 			{/* Fourth Tab - Contact Tab */}
