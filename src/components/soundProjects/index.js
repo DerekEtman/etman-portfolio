46defbf5f1753fb1cff69157d4f71010a48157e0
Derek Etman <dereketman@Gmail.com>
2022-06-17 09:47:15 -0700

    Sound page cards

--- src/components/soundProjects/index.js
+++ src/components/soundProjects/index.js
@@ -1,16 +1,25 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Box, Grid, ImageList, ImageListItem, makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Grid,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  makeStyles,
+} from "@material-ui/core";
 import PianoImage from "./assets/piano.jpg";
 import useContentful from "../../hooks/useContentful";
 import { CompareSharp } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    width:"100%",
+    width: "90%",
     backgroundColor: "Black",
     color: "white",
     minHeight: "100vh",
     fontFamily: "'Bebas Neue', cursive",
+    margin:"3vh auto",
+    // overflow:"hidden",
     // backgroundImage: `url(${PianoImage})`,
     // backgroundSize: "300%",
     // backgroundPosition: "center",
@@ -21,22 +30,43 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// function checkContentType(asset) {
+//   const {file:{contentType}} = asset.fields
+//   switch(contentType){
+//     case "audio/":
+
+//   }
+// }
+
 function createCard(asset) {
+  console.log("Asset", asset);
   const {
     title,
     file: { contentType, fileName, url },
     thumbnailImage,
-  } = asset.fields;
+  } = asset?.fields;
 
-  const{ file:{url: thubnailURL}} = thumbnailImage.fields
-
-  console.log("create card", thumbnailImage.fields);
+  const {
+    // file: {
+    url: thumbnailURL,
+    // },
+  } = thumbnailImage?.fields?.file;
 
   return (
-    <ImageListItem key={url}>
-      {/* <p>{title}</p> */}
-      <img src={thubnailURL} alt={fileName}/>
+    // <div>
+    // {thumbnailURL ? (
+    <ImageListItem key={url} >
+      <img src={thumbnailURL} alt={fileName} loading="lazy" />
+      <ImageListItemBar title={title} subtitle={contentType}  />
+      {/* <h5>{title}</h5> */}
     </ImageListItem>
+    // ) : (
+    //   <div style={{ border: "1px solid white" }}>
+    //     <h3>{title}</h3>
+    //     <h4>{contentType}</h4>
+    //   </div>
+    // )}
+    // </div>
   );
 }
 
@@ -66,22 +96,22 @@ export default function SoundPage() {
   const classes = useStyles();
 
   return (
-    <Box>
-    <Grid container className={classes.container}>
-      <Grid item xs={2}>
-        Filter Nav
+    // <Box>
+      <Grid container className={classes.container}>
+        {/* <Grid item xs={2}>
+          Filter Nav
+        </Grid> */}
+        <Grid item xs={12}>
+          <ImageList
+            // sx={{ width: 500, height: 450 }}
+            variant="quilted"
+            cols={3}
+            rowHeight={121}
+          >
+            {entriesList.map((asset) => createCard(asset))}
+          </ImageList>
+        </Grid>
       </Grid>
-      <Grid item xs={10}>
-        <ImageList
-          sx={{ width: 500, height: 450 }}
-          variant="quilted"
-          cols={4}
-          rowHeight={121}
-        >
-          {entriesList.map((asset) => createCard(asset))}
-        </ImageList>
-      </Grid>
-    </Grid>
-    </Box>
+    // </Box>
   );
 }
